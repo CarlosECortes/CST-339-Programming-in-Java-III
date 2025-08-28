adb1535b82d40c9daa55ced8ebec623ae971ed35
Carlos Cortes <138523956+CarlosECortes@users.noreply.github.com>
2025-08-27 19:52:01 -0700

    Add files via upload

---
+++ topic1-1/src/main/java/com/gcu/Topic11Application.java
@@ -0,0 +1,14 @@
+package com.gcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Topic11Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World from Topic1-1!");
+		SpringApplication.run(Topic11Application.class, args);
+	}
+
+}
